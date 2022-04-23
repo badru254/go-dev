a6ef3e6913ef9ee19203f1ffeeaeba444289b389
Badru254 <suleybadr@yahoo.com>
2022-04-24 02:24:23 +0300

    Inputs

--- practice/main.go
+++ practice/main.go
@@ -1,38 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-const aConst int = 645
-
 func main() {
-	//Explicit Typing
-	var aString string = "This is a go var"
-	fmt.Println(aString)
-	fmt.Printf("The variable type is %T\n", aString)
 
-	var anInteger int = 45
-	fmt.Println(anInteger)
+	//Stdin = standard input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
 
-	var defaultInt int
-	fmt.Println(defaultInt)
+	fmt.Println("You entered:", input)
 
-	//Implicit Typing
-	var anotherString = "This is another gor var"
-	fmt.Println(anotherString)
-	fmt.Printf("The variable type is %T\n", anotherString)
-
-	var anotherInt = 767
-	fmt.Println(anotherInt)
-	fmt.Printf("The variable type is %T\n", anotherInt)
-
-	//Colon-equals operator
-	myString := "this is also a string"
-	fmt.Println(myString)
-	fmt.Printf("The variable type is %T\n", myString)
-
-	//Constant
-	fmt.Println(aConst)
-	fmt.Printf("The variable type is %T\n", aConst)
 }
