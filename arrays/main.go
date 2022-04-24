3194295bddeb4eddfa7d5fbf8e7fa23e62326847
Badru254 <suleybadr@yahoo.com>
2022-04-24 22:53:59 +0300

    Arrays

--- arrays/main.go
+++ arrays/main.go
@@ -5,7 +5,16 @@ import (
 )
 
 func main() {
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
 
-	fmt.Println("Arrays")
+	fmt.Println(colors)
 
+	var numbers = [5]int{23, 34, 45, 44, 34}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Numbers of integers:", len(numbers))
 }
